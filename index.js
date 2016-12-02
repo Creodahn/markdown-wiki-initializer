ec0511043a0eb4d23a261c97a7af12615c6ed34c
Justin Drew <jdrew@sca-corp.com>
2016-12-02 10:20:02 -0500

    rename root markdown file to Home.md on exit to follow Bitbucket conventions; delete any existing Home.md file created by Bitbucket; skip both hidden files and directories; fix a problem where multiple copies of the root markdown file would be created in various places

--- index.js
+++ index.js
@@ -5,18 +5,26 @@ const fs = require('graceful-fs'),
 const args = require('minimist')(process.argv.slice(2));
 
 if(args.src && args.target) {
-  handleDirectory(args.src, args.target, '', args.src.replace(path.dirname(args.src) + '\/', ''));
+  let item = args.src.replace(path.dirname(args.src) + '\/', '');
+  handleDirectory(args.src, args.target, '', item);
+  process.on('exit', function() {
+    if(checkFileExistence(args.target, 'Home.md')) {
+      fs.unlinkSync(path.resolve(args.target, 'Home.md'));
+    }
+
+    fs.renameSync(path.resolve(args.target, `${item}.md`), path.resolve(args.target, `Home.md`));
+  });
+
 } else {
   console.error('Missing one or both of required arguments: --src and --target');
 }
-
 function handleDirectory(dir, targetDir, relPath, item) {
-  let tFile = `${item}.md`;
+  let tFile = path.resolve(targetDir, `${item}.md`);
 
   makeDirectory(targetDir, item);
   makeMarkdownFile(targetDir, item);
   writeMarkdown(tFile, `# ${item}\n\n`);
-  parseDirectory(dir, path.resolve(targetDir, item), path.join(relPath, item), path.resolve(targetDir, tFile));
+  parseDirectory(dir, path.resolve(targetDir, item), path.join(relPath, item), tFile);
 }
 
 function makeDirectory(loc, item) {
@@ -46,18 +54,21 @@ function parseDirectory(dir, targetDir, relPath, targetFile) {
           disallowedDirs = ['node_modules', 'bower_components'],
           isDir = chk.isDirectory() && disallowedDirs.indexOf(item.toLowerCase()) < 0,
           //check if is file and is not a hidden file
-          isFile = chk.isFile() && item.indexOf('.') !== 0,
+          isFile = chk.isFile(),
+          isHidden = item.indexOf('.') === 0,
           markdown = `*   [${item}](${relPath}/${removeExtension(item)}.md)\n\n`;
 
-      switch(true) {
-        case isDir:
-          writeMarkdown(targetFile, markdown);
-          handleDirectory(path.resolve(dir, item), targetDir, relPath, item);
-          break;
-        case isFile:
-          makeMarkdownFile(targetDir, removeExtension(item));
-          writeMarkdown(targetFile, markdown);
-          break;
+      if(!isHidden) {
+        switch(true) {
+          case isDir:
+            writeMarkdown(targetFile, markdown);
+            handleDirectory(path.resolve(dir, item), targetDir, relPath, item);
+            break;
+          case isFile:
+            makeMarkdownFile(targetDir, removeExtension(item));
+            writeMarkdown(targetFile, markdown);
+            break;
+        }
       }
     });
   });
